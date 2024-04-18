9cb5b343d6eaf3e55726468e7ff1bd91845a048e
luoyihao <13324612605@qq.com>
2024-04-18 23:34:57 +0800

    first commit

---
+++ connectivity.dart
@@ -0,0 +1,92 @@
+// ignore_for_file: avoid_print
+
+import 'dart:async';
+import 'dart:developer' as developer;
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<ConnectivityResult> _connectionStatus = [ConnectivityResult.none];
+  final Connectivity _connectivity = Connectivity();
+  late StreamSubscription<List<ConnectivityResult>> _connectivitySubscription;
+
+  @override
+  void initState() {
+    super.initState();
+    initConnectivity();
+
+    _connectivitySubscription =
+        _connectivity.onConnectivityChanged.listen(_updateConnectionStatus);
+  }
+
+  @override
+  void dispose() {
+    _connectivitySubscription.cancel();
+    super.dispose();
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> initConnectivity() async {
+    late List<ConnectivityResult> result;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      result = await _connectivity.checkConnectivity();
+    } on PlatformException catch (e) {
+      developer.log('Couldn\'t check connectivity status', error: e);
+      return;
+    }
+
+    if (!mounted) {
+      return Future.value(null);
+    }
+
+    return _updateConnectionStatus(result);
+  }
+
+  Future<void> _updateConnectionStatus(List<ConnectivityResult> result) async {
+    setState(() {
+      _connectionStatus = result;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Connectivity Plus 示例'),
+        elevation: 4,
+      ),
+      body: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const Spacer(flex: 2),
+          Text(
+            '连接类型',
+            style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          const Spacer(),
+          ListView(
+            shrinkWrap: true,
+            children: List.generate(
+                _connectionStatus.length,
+                    (index) => Center(
+                  child: Text(
+                    _connectionStatus[index].toString(),
+                    style: Theme.of(context).textTheme.headlineSmall,
+                  ),
+                )),
+          ),
+          const Spacer(flex: 2),
+        ],
+      ),
+    );
+  }
+}
